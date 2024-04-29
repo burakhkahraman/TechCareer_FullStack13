9a1bab8ea07f2a3b5c85728c2de715a00c82067a
Burak Kahraman <burakhasankahraman@gmail.com>
2024-04-29 12:16:21 +0300

    Java

--- src/main/java/org/burakkahraman/_1_WhatIsJava.java
+++ src/main/java/org/burakkahraman/_1_WhatIsJava.java
@@ -3,5 +3,23 @@ package org.burakkahraman;
 public class _1_WhatIsJava {
     public static void main(String[] args) {
         System.out.println("Merhabalar, nasılsınız?");
+
+        //Variable
+        // Tam Sayılar
+        byte b1= 25;
+        System.out.println(b1);
+        short s1=125;
+        int i1= 45648;
+        long l1= 46545646865644L;
+
+        //Virgüllü sayılar
+        float f2= 14.25f;
+        double d2 = 1315313.2626156;
+
+        char c3= '2';
+        boolean b3= 3>1;
+        System.out.println(b3);
+
+        //primitive type- Wrapper
     }
 }
