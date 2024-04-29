915b0535056a15fb106959580b059574c39daeca
Burak Kahraman <burakhasankahraman@gmail.com>
2024-04-29 12:06:01 +0300

    Java

--- src/main/java/org/burakkahraman/_1_WhatIsJava.java
+++ src/main/java/org/burakkahraman/_1_WhatIsJava.java
@@ -1,2 +1,7 @@
-package org.burakkahraman;public class _1_WhatIsJava {
+package org.burakkahraman;
+
+public class _1_WhatIsJava {
+    public static void main(String[] args) {
+        System.out.println("Merhabalar, nasılsınız?");
+    }
 }
