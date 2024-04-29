4ed14fb7036ac64342c157efbe42e9ae9ac6970e
Burak Kahraman <burakhasankahraman@gmail.com>
2024-04-29 14:18:10 +0300

    Conditional, Date

--- src/main/java/org/burakkahraman/_8_Date.java
+++ src/main/java/org/burakkahraman/_8_Date.java
@@ -1,4 +1,26 @@
 package org.burakkahraman;
 
+import java.util.Date;
+
 public class _8_Date {
+
+
+    public static void main(String[] args) {
+        Date date=new Date();
+        System.out.println(date);
+        System.out.println(date.getTime());
+        System.out.println(date.getDay());
+        System.out.println(date.getHours());
+        System.out.println(date.getMinutes());
+        System.out.println(date.getHours()+":"+date.getMinutes());
+        date.setHours(17);
+        System.out.println(date.getHours()+":"+date.getMinutes());
+        System.out.println(date.getHours()+":"+date.getMinutes()+" "+date.getYear());
+
+    }
+
+
+    // Stack Memory Heap Memory Araştırma
+    // Date 2024 iken bana 124 sayısını veriyor  1900 ?
+
 }
